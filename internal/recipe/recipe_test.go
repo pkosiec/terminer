4ba06fa6d17ef57715a8c8cc22e69d6f4ca7fd8c
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-02-04 23:58:18 +0100

    Add ability to download recipe from URL (#18)
    * Add ability to download recipe from URL
    
    * Write additional unit tests

--- internal/recipe/recipe_test.go
+++ internal/recipe/recipe_test.go
@@ -4,41 +4,116 @@ import (
 	"github.com/pkosiec/terminer/internal/recipe"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"runtime"
 	"testing"
 )
 
-func TestFrom(t *testing.T) {
+func TestFromPath(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		r, err := recipe.From("./fixture/valid-recipe.yaml")
 		expected := fixRecipe("testos")
+
+		r, err := recipe.FromPath("./fixture/valid-recipe.yaml")
+
 		require.NoError(t, err)
 		assert.Equal(t, expected, r)
 	})
 
 	t.Run("Invalid Path", func(t *testing.T) {
-		_, err := recipe.From("./fixture/no-file-exist.yaml")
+		_, err := recipe.FromPath("./fixture/no-file-exist.yaml")
+
 		require.Error(t, err)
 		require.Contains(t, err.Error(), "no such file")
 	})
 
 	t.Run("Invalid File", func(t *testing.T) {
-		_, err := recipe.From("./fixture/invalid-recipe.yaml")
+		_, err := recipe.FromPath("./fixture/invalid-recipe.yaml")
+
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "while loading recipe")
+	})
+}
+
+func TestFromURL(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		expected := fixRecipe("testos")
+		server := setupRemoteRecipeServer(t, "./fixture/valid-recipe.yaml", false)
+		defer server.Close()
+
+		r, err := recipe.FromURL(server.URL)
+
+		require.NoError(t, err)
+		assert.Equal(t, expected, r)
+	})
+
+	t.Run("Not existing path", func(t *testing.T) {
+		_, err := recipe.FromURL("http://foo-bar.not-existing.url")
+
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "while requesting")
+	})
+
+	t.Run("Server Error", func(t *testing.T) {
+		server := setupRemoteRecipeServer(t, "", true)
+		defer server.Close()
+
+		_, err := recipe.FromURL(server.URL)
+
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "Invalid status code")
+	})
+
+	t.Run("Error during reading response body", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Length", "1")
+		}))
+		defer server.Close()
+
+		_, err := recipe.FromURL(server.URL)
+
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "while reading response body")
+	})
+
+	t.Run("Empty response body", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+		}))
+		defer server.Close()
+
+		_, err := recipe.FromURL(server.URL)
+
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "Empty body")
+	})
+
+	t.Run("Invalid File", func(t *testing.T) {
+		server := setupRemoteRecipeServer(t, "./fixture/invalid-recipe.yaml", false)
+		defer server.Close()
+
+		_, err := recipe.FromURL(server.URL)
+
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "while loading recipe from URL")
 	})
 }
 
 func TestValidate(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		r := fixRecipe(runtime.GOOS)
+
 		err := r.Validate()
+
 		assert.NoError(t, err)
 	})
 
 	t.Run("Invalid OS", func(t *testing.T) {
 		r := fixRecipe("notexistingos")
+
 		err := r.Validate()
+
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "Invalid operating system")
 	})
@@ -48,7 +123,9 @@ func TestValidate(t *testing.T) {
 			OS:   runtime.GOOS,
 			Name: "Test",
 		}
+
 		err := r.Validate()
+
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "No stages")
 	})
@@ -72,7 +149,9 @@ func TestValidate(t *testing.T) {
 				},
 			},
 		}
+
 		err := r.Validate()
+
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "No steps")
 	})
@@ -92,7 +171,9 @@ func TestValidate(t *testing.T) {
 				},
 			},
 		}
+
 		err := r.Validate()
+
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "No command")
 	})
@@ -145,3 +226,22 @@ func fixRecipe(os string) *recipe.Recipe {
 		},
 	}
 }
+
+func setupRemoteRecipeServer(t *testing.T, recipePath string, returnError bool) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if returnError {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, err := w.Write([]byte("Internal Server Error"))
+			require.NoError(t, err)
+			return
+		}
+
+		yamlFile, err := ioutil.ReadFile(recipePath)
+		require.NoError(t, err)
+		_, err = w.Write(yamlFile)
+		require.NoError(t, err)
+	}))
+
+	return server
+}
+
