42e6babd91244fbb67f3697ed8a4ca39847c2933
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-02-10 18:09:29 +0100

    Install cobra

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"github.com/pkosiec/terminer/cmd"
-	"log"
 )
 
 func main() {
 	cmd.Execute()
-}
-
-func exitOnError(err error) {
-	if err == nil {
-		return
-	}
-
-	log.Fatal(err)
 }
